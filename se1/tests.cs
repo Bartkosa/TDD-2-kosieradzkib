37e6010120e0440acea36adac4b0cb936f704dc5
Bartosz Kosieradzki <115025011+Bartkosa@users.noreply.github.com>
2023-03-15 21:47:04 +0100

    Add files via upload

---
+++ se1/tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace se1
+{
+    internal class tests
+    {
+        public tests()
+        {
+            functions functions = new functions();
+
+            Console.WriteLine(Test_add(functions));
+
+        }
+
+        static bool Test_add(functions functions)
+        {
+            // Given
+            string numbers = "//[***][;;]\n22;;13***20";
+            int expected_value = 55;
+            int result;
+
+            // When
+            if (String.IsNullOrEmpty(numbers))
+            {
+                Console.WriteLine("empty string");
+                return false;
+            }
+
+            result = functions.Add(numbers);
+
+            // Then
+            return result.Equals(expected_value);
+        }
+    }
+}
